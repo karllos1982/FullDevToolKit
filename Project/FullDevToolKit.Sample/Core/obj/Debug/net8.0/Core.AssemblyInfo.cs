cb99f529be0955ec4ea1afa8f3649169b79a60e2
Carlos Fonteles <karllos1982@hotmail.com>
2025-10-19 21:58:22 -0300

    Ajustes na tela de login
    Continuação do desenvolvimento da V2.

--- Project/FullDevToolKit.Sample/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Project/FullDevToolKit.Sample/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21c300ad1b67a0056d2b5989abb710a23579519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f3acbb60e1f8d7555cc2ca3b7130b1543f4e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project/FullDevToolKit.Sample/Proxy/obj/Debug/net8.0/Proxy.AssemblyInfo.cs
+++ Project/FullDevToolKit.Sample/Proxy/obj/Debug/net8.0/Proxy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proxy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21c300ad1b67a0056d2b5989abb710a23579519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f3acbb60e1f8d7555cc2ca3b7130b1543f4e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proxy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proxy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
