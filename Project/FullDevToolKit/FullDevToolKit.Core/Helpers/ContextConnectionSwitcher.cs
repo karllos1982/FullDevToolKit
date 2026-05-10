8101e993fc68857636a93038c549d280ce7bb139
Carlos Fonteles <12647791+karllos1982@users.noreply.github.com>
2026-05-10 20:09:38 -0300

    Implementacao da ContextConnectionSwitcher
    Classe para controle e alternância da conexão com o banco de dados individualmente nos endpoints da API.

---
+++ Project/FullDevToolKit/FullDevToolKit.Core/Helpers/ContextConnectionSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullDevToolKit.Core.Helpers
+{
+   
+    public class ContextConnectionSwitcher
+    {
+        private Dictionary<string, ConnectionStringItem> Connections 
+                = new Dictionary<string, ConnectionStringItem>();
+
+        public ContextConnectionSwitcher()
+        {
+
+        }
+
+        public void AddDefault(ConnectionStringItem conn)
+        {
+            Connections.Add("DEFAULT", conn);
+
+        }
+
+        public void AddConnection(string name, ConnectionStringItem conn)
+        {
+            Connections.Add(name, conn);
+        }
+
+        public ConnectionStringItem GetConnection(string name)
+        {
+            if (Connections.ContainsKey(name))
+            {
+                return Connections[name];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+   
+}
